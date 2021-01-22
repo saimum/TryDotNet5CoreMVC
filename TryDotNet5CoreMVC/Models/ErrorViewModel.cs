ed7c35a02068933b4c1f7f82f90fcd86f9136090
saimum <saimum720@gmail.com>
2021-01-22 20:00:52 +0600

    Add project files.

---
+++ TryDotNet5CoreMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TryDotNet5CoreMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
